357cd467009a8488b202f5fdfe02467faca6aae2
Jad <jad@videolab.ae>
2019-02-16 15:04:23 +0400

    2: Setting up Functions

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import sys
 
+class Operation:
+	def __init__(self, operation, numbers):
+		self.operation = operation;
+		self.numbers = numbers;
+
+def introduction():
+	pass
+
+def print_instructions():
+	pass
+
+def get_user_input():
+	pass
+
+def parse_input(string):
+	pass
+
+def run_operation(operation):
+	pass
+
+def answer_user(answer):
+	pass
+
+def ask_again():
+	pass
+
+def main():
+	pass
+
+
+main()

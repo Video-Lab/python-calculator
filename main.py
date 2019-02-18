3ac8cef6f538d89a9b8ba507e8f14d1360b83323
Jad <jad@videolab.ae>
2019-02-18 13:11:17 +0400

    4: User Input & Parsing

--- main.py
+++ main.py
@@ -15,10 +15,27 @@ def print_instructions():
 	print(instructions)
 
 def get_user_input():
-	pass
+	user_input = input("Enter your command: ")
+	return user_input
 
-def parse_input(string):
-	pass
+def parse_input(user_input):
+# add 30,50 ---> 80
+# ^ Op. ^Num. list
+# ["a","b","c"] <-- List
+
+# Split string into op. and numbers
+	user_input_parsed = user_input.split(" ")
+	# ["add", "30,50"]
+# Make list of numbers (that is easy to work with)
+	user_input_numbers = user_input_parsed[1].split(",")
+	# ["30", "50"]
+	for number in user_input_numbers:
+		number = int(number)
+	# [30, 50]
+
+# Create instance of operation class & return it
+	operation = Operation(user_input_parsed[0], user_input_numbers)
+	return operation
 
 def run_operation(operation):
 	pass
@@ -34,4 +51,6 @@ def main():
 
 
 # main()
-print_instructions()
+user_input = get_user_input()
+operation = parse_input(user_input)
+print(operation.numbers)
